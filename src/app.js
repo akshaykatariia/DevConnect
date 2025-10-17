a417c4f761c0a1ccdda7524d385526a3ee4fb548
akshaykatariia <akshaykataria055@gmail.com>
2025-10-17 15:04:24 +0530

    added schema model and necessary required checks

--- src/app.js
+++ src/app.js
@@ -1,19 +1,95 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard!");
+app.use(express.json()); // middleware for json to js obj conversion
+
+app.get("/signup", async (req, res) => {});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const email = req.query.email;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("FETCH_FAILED", err.message);
+  }
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("FETCH_FAILED", err.message);
+  }
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
+// Delete a user from the database
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete({ _id: userId });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("DELETE_FAILED", err.message);
+  }
 });
 
-app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777...");
+// Update data of the user
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE_FAILED", err.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
